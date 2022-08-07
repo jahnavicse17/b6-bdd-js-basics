a8a324b47caca090375f531b7c59a144215faca2
Jahnavi <jahnavis17@gmail.com>
2022-08-07 15:39:17 -0400

    jsbasics was added

---
+++ js-basics.js
@@ -0,0 +1,28 @@
+
+console.log("Hello World!")
+
+let a=20;
+let b=20;
+console.log(a+b);
+
+let c="123";
+let d=123;
+if(c==d)
+{
+console.log("True");
+}
+else
+{
+    console.log("False");
+}
+
+for(let e=1;e<=10;e++)
+{
+    console.log(e);
+}
+
+let f=20;
+let g=30;
+console.log((a+f)*g);
+
+
